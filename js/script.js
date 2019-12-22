0676ac9644593e9592f8941ea39c5d9e8d9404f6
VeronikaVasilchikova <veroni4ka.v@gmail.com>
2019-12-22 21:23:24 +0300

    Create GET-request with promise

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ let inputRub = document.getElementById('rub'),
 
 // the first way without promise
 
+/*
 inputRub.addEventListener('input', () => {
     let request = new XMLHttpRequest();
 
@@ -20,6 +21,37 @@ inputRub.addEventListener('input', () => {
         }
     });
 
+});
+*/
+
+// the second way with promise
+
+function getData(url) {
+    return new Promise(function(resolve, reject) {
+        let request = new XMLHttpRequest();
+        request.open('GET', url);
+        request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+
+        request.addEventListener('load', function() {
+            if (request.readyState === 4 && request.status == 200) {
+                resolve(request.response);
+            } else {
+                let error = new Error(request.statusText);
+                reject(error);
+            }
+        });   
+        request.send();  
+    });
+}
+
+inputRub.addEventListener('input', function() {
+    getData('js/current.json')
+        .then(function(response) {
+            inputUsd.value = (inputRub.value / (JSON.parse(response)).usd).toFixed(2);
+        })
+        .catch(function(error){
+            inputUsd.value = error;
+        });
 });
 
 
